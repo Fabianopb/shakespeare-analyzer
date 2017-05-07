5e925307e7040becd9664bb25fd2278cc5b5b207
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-07 14:47:50 +0300

    Parses the XML and displays number of lines spoken in a list.

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -4,7 +4,18 @@ class AnalyzerController < ApplicationController
   end
 
   def results
-    @test = 'some result will appear here'
+    @speechCount = Hash.new
+
+    doc = Nokogiri::XML(open('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'))
+
+    speeches = doc.css('SPEECH')
+    speeches.each do |speech|
+      speaker = speech.at_css('SPEAKER').content
+      lines = speech.css('LINE')
+      if speaker != 'ALL'
+        @speechCount[speaker] = @speechCount[speaker].nil? ? lines.size : @speechCount[speaker] + lines.size
+      end
+    end
   end
 
 end
--- test/controllers/analyzer_controller_test.rb
+++ test/controllers/analyzer_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class AnalyzerControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index url" do
+  test 'should get index url' do
     get analyzer_index_url
     assert_response :success
   end
 
-  test "should post results url" do
+  test 'should post results url' do
     post analyzer_results_url
     assert_response :success
   end
