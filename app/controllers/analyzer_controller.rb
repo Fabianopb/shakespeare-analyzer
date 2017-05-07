5bca21824581f5b538a5ccc41fadaba67c844660
Fabiano Brito <fabianopbrito@gmail.com>
2017-05-07 15:02:06 +0300

    Uses key and value to iterate over the speech count hash.

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -4,11 +4,14 @@ class AnalyzerController < ApplicationController
   end
 
   def results
+    
     @speechCount = Hash.new
 
     doc = Nokogiri::XML(open('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'))
 
     speeches = doc.css('SPEECH')
+    totalLines = doc.css('LINE')
+    puts totalLines.size
     speeches.each do |speech|
       speaker = speech.at_css('SPEAKER').content
       lines = speech.css('LINE')
@@ -16,6 +19,7 @@ class AnalyzerController < ApplicationController
         @speechCount[speaker] = @speechCount[speaker].nil? ? lines.size : @speechCount[speaker] + lines.size
       end
     end
+
   end
 
 end
